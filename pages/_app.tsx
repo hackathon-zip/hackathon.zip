8b715034dfbefd7dbeed0ba5165b10cfb44c024e
Yoda <yoda@yodacode.xyz>
2023-09-21 20:41:27 -0700

    Hackathon Thing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
+import { CssBaseline, GeistProvider } from '@geist-ui/core';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GeistProvider>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </GeistProvider>
+  );
 }
