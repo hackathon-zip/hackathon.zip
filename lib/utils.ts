ef4d788883fa022c90a43016df87a16f167fd16a
Jack Merrill <me@jackmerrill.com>
2023-10-02 15:17:49 -0400

    WIP

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,6 @@
+import { Hackathon } from "@prisma/client";
+import prisma from "./prisma";
+
 export const delay = (ms: number): Promise<void> =>
     new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -5,7 +8,7 @@ type Params = Record<string, unknown>;
 
 export function permitParams<T extends Params>(
     allowedKeys: (keyof T)[],
-    params: T,
+    params: T
 ): T {
     const permittedParams = {} as T;
 
@@ -16,4 +19,39 @@ export function permitParams<T extends Params>(
     }
 
     return permittedParams;
+}
+
+export function getSubdomains(host: string | null): string[] | undefined {
+    // supports localhost subdomains
+    if (!host) {
+        return undefined;
+    }
+    const parts = host.split(".");
+
+    if (process.env.NODE_ENV === "development" || host.includes("localhost")) {
+        return parts.slice(0, parts.length - 1);
+    }
+
+    if (parts.length <= 2) {
+        return [];
+    }
+
+    return parts.slice(0, parts.length - 2);
+}
+
+export async function getHackathonBySubdomain(
+    host: string
+): Promise<Hackathon> {
+    const subdomains = getSubdomains(host);
+    const hackathon = await prisma.hackathon.findUnique({
+        where: {
+            slug: subdomains?.[0],
+        },
+    });
+
+    if (!hackathon) {
+        throw new Error("Hackathon not found");
+    }
+
+    return hackathon;
 }
--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { getSubdomains } from "./lib/utils";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -7,7 +8,7 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 export default function middleware(
     request: NextRequest,
-    event: NextFetchEvent,
+    event: NextFetchEvent
 ) {
     const withAuthentication = (callback: (req: NextRequest) => any) => {
         return authMiddleware({
@@ -20,7 +21,7 @@ export default function middleware(
 
     const withoutAuthentication = (
         route: string,
-        callback: (req: NextRequest) => any,
+        callback: (req: NextRequest) => any
     ) => {
         return authMiddleware({
             publicRoutes: [route],
@@ -30,7 +31,8 @@ export default function middleware(
         })(request, event);
     };
 
-    const { hostname, pathname } = request.nextUrl;
+    const { pathname } = request.nextUrl;
+    const hostname = request.headers.get("host"); // Get the hostname from the request headers, because Next.js doesn't like localhost subdomains (vercel/next.js#56320)
     const isApi = pathname.startsWith("/api");
     const isAttendeeApi = pathname.startsWith("/api/attendee/");
     let pathnameWithoutAPI = pathname.replace("/api", "");
@@ -40,28 +42,28 @@ export default function middleware(
 
     let subdomain: string | undefined;
 
-    if (hostname.endsWith("hackathon.zip"))
-        subdomain = hostname.split(".").reverse()[2];
-    if (hostname.endsWith("localhost")) subdomain = undefined;
-
+    // if (hostname.endsWith("hackathon.zip"))
+    subdomain = getSubdomains(hostname)?.[0];
+    // if (hostname.endsWith("localhost")) subdomain = undefined;
+    console.log(subdomain, hostname, request.nextUrl.host);
     switch (subdomain) {
         case undefined: // you are on hackathon.zip
             return withAuthentication(
                 isApi && !isAttendeeApi
                     ? () => rewrite("/api/organizer" + pathnameWithoutAPI)
-                    : () => null,
+                    : () => null
             );
         case "api": // you are on api.hackathon.zip
             return withoutAuthentication(pathname, () =>
-                rewrite("/api/integration" + pathname),
+                rewrite("/api/integration" + pathname)
             );
         default: // you are on [event].hackathon.zip
             return withoutAuthentication(pathname, () =>
                 rewrite(
                     isApi
                         ? `/api/attendee/${subdomain}` + pathnameWithoutAPI
-                        : `/attendee/${subdomain}` + pathname,
-                ),
+                        : `/attendee/${subdomain}` + pathname
+                )
             );
     }
 }
@@ -93,6 +95,21 @@ export const config = {
  *
  * [event].hackathon.zip/route -> /attendee/[event]/route
  * [event].hackathon.zip/api/route -> /api/attendee/[event]/route
+ *
+ * api.hackathon.zip/route -> /api/integration/route
+ */
+
+/**
+ * Tertiary Plan:
+ *
+ * hackathon.zip/route -> /route (LANDING PAGE)
+ * hackathon.zip/api/route -> /api/route
+ *
+ * [event].hackathon.zip/route -> /attendee/[event]/route
+ * [event].hackathon.zip/api/route -> /api/attendee/[event]/route
+ *
+ * [event].hackathon.zip/organizer/route -> /organizer/[event]/route
+ * [event].hackathon.zip/api/organizer/route -> /api/organizer/[event]/route
  *
  * api.hackathon.zip/route -> /api/integration/route
  */
